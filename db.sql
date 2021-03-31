a12e26ee99b04c7ead2bdcb3e15779dd10bbbf6a
logeable <logeable@gmail.com>
2021-04-01 07:37:55 +0800

    fix: add index to user table

--- db.sql
+++ db.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS "user" (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(128),
-    password VARCHAR(128)
-)
+    name VARCHAR(128) NOT NULL,
+    password VARCHAR(128) NOT NULL
+);
+
+CREATE UNIQUE INDEX unique_user_name ON "user" (name);
